129c31cf48cabd718e15fde660d936700f187887
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-31 10:25:34 -0700

    Add listings index route to controller

--- controllers/listings.js
+++ controllers/listings.js
@@ -3,6 +3,15 @@ const router = express.Router();
 
 const Listing = require('../models/listing');
 
-
+router.get('/', async (req, res) => {
+    try {
+        const allListings = await Listing.find();
+        console.log(allListings);
+        res.send('listings index page');
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    };
+});
 
 module.exports = router;
